cbad6268ab03380e68d4b987379a870a03e53652
casaumayman <casaumayman1@gmail.com>
2018-10-26 19:20:15 +0700

    updated

--- core/them-vao-gio-hang.php
+++ core/them-vao-gio-hang.php
@@ -3,14 +3,13 @@
 	$id_nguoidung = $_SESSION['id'];
 	$ngaymua = date("Y-m-d");
 	$id_sanpham = addslashes($_GET['id-xe']);
-	//$sl = addslashes($_POST['sl']);
-	$sl = 1;
+	$sl = addslashes($_POST['sl']);
 	$trangthai = "Ðã nhận đơn hàng";
 
 	if($sl==0) $sl=1;
 
 	// kiem tra xem ma hang da dat mua chua?
-	if ($db->query("SELECT * FROM cart_details WHERE id_products = {$id_sanpham}")->fetch())
+	if ($db->query("SELECT * FROM carts WHERE id_products = {$id_sanpham}")->fetch())
 	{
 		echo"<SCRIPT LANGUAGE='JavaScript'>alert('Sản phẩm này đã được đặt!'); </script>";
 		echo"<SCRIPT LANGUAGE='JavaScript'> history.go(-1); </script>";
